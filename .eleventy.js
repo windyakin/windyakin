70e34e38613ebc6a692bd0ae1722dfa6d6272fe5
windyakin <windyakin@gmail.com>
2025-06-25 19:14:14 +0900

    バスの貸し切りについての記事

--- .eleventy.js
+++ .eleventy.js
@@ -66,6 +66,19 @@ module.exports = function (eleventyConfig) {
       class: "responsive-img",
       decoding: "async"
     },
+    renderImage(image, attributes) {
+      const [ Image, options ] = image;
+      const [ src, attrs ] = attributes;
+
+      Image(src, options);
+
+      const metadata = Image.statsSync(src, options);
+      const imageMarkup = Image.generateHTML(metadata, attrs, {
+        whitespaceMode: "inline"
+      });
+
+      return `<figure>${imageMarkup}${attrs.title ? `<figcaption>${attrs.title}</figcaption>` : ""}</figure>`;
+    }
   });
   eleventyConfig.setLibrary("md", markdownLibrary);
 };
