8ac0b7271a66b1cc8d08df3e0081178ec069bd2e
windyakin <windyakin@gmail.com>
2026-08-21 13:32:11 +0900

    minify する

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import compress from '@playform/compress';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { rehypeFigure } from './src/plugins/rehype-figure.mjs';
 
 export default defineConfig({
+  integrations: [compress()],
   site: 'https://windyakin.net',
   build: {
     format: 'preserve',
